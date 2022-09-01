cb44e6a0e99dd211d01a5d98393666f4d6734a6f
cevik <suleymancevik64@gmail.com>
2022-09-01 03:37:37 +0300

    22-09-01

---
+++ src/main/java/jdbc/Execute01.java
@@ -0,0 +1,55 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Execute01 {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        // ---------- 1.ADIM => Driver'a KAYDOL --------------
+        Class.forName("org.postgresql.Driver");
+
+
+        // ---------- 2.ADIM => Database'e BAGLAN ------------
+        Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/techproed", "postgres", "690101");
+
+
+        // ---------- 3.ADIM => Statement OLUSTUR ------------
+        Statement st = con.createStatement();
+
+
+        // ---------- 4.ADIM => Query CALISTIR ----------------
+
+        // TASK-1 : "workers" adında bir table olusturup "worker_id, worker_name, worker_salary" sutunlarini ekleyin.
+
+        String sql1 = "CREATE TABLE workers (workers_id VARCHAR(50), worker_name VARCHAR(50), worker_salary INT)";
+
+        boolean result = st.execute(sql1);
+
+        //System.out.println(result); // false return eder, data cagrilmadi icin
+
+        // TASK-2 : workers table'a worker_address sutunu ekleyerek alter yapin.
+
+        String sql2 = "ALTER TABLE workers ADD worker_address VARCHAR(80)";
+
+        st.execute(sql2);
+
+
+        // TASK-3 : Drop workers table
+
+        String sql3 = "DROP TABLE workers";
+
+        st.execute(sql3);
+
+
+        // ------------ 5.ADIM => Baglanti ve Statement'i KAPAT --------------
+
+        con.close(); // guvenlik amacli kapatilir.
+        st.close();
+
+
+    }
+}
---
+++ src/main/java/jdbc/Execute02.java
@@ -0,0 +1,61 @@
+package jdbc;
+
+import java.sql.*;
+
+public class Execute02 {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        Class.forName("org.postgresql.Driver");
+
+        Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/techproed", "postgres", "690101");
+
+        Statement st = con.createStatement();
+
+
+        // TASK-1 :  region_id'si 1 olan "country name" degerlerini cagirin.
+
+        String sql1 = "SELECT country_name FROM countries WHERE region_id = 1";
+
+
+        // NOTE => record'lari gormek icin executeQuery() method'u kullanmaliyiz.
+
+        ResultSet rs1 = st.executeQuery(sql1);
+
+        while (rs1.next()) {
+
+            System.out.println(rs1.getString("country_name"));
+        }
+
+
+        // TASK-2 : "region_id" nin 2'den buyuk oldugu "country_id" ve "country_name" degerlerini cagirin.
+
+        String sql2 = "SELECT country_id, country_name FROM countries WHERE region_id > 2";
+
+        ResultSet rs2 = st.executeQuery(sql2);
+
+        while (rs2.next()) {
+
+            System.out.println(rs2.getString("country_id") + " => " + rs2.getString("country_name"));
+        }
+
+
+        // TASK-3 : "number_of_employees" degeri en dusuk olan satirin tum degerlerini cagirin.
+
+        String sql3 = "SELECT * FROM companies WHERE number_of_employees = (SELECT MIN(number_of_employees) FROM companies)";
+
+        ResultSet rs3 = st.executeQuery(sql3);
+
+        while (rs3.next()) {
+
+            System.out.println(rs3.getInt("company_id") + " => " +
+                    rs3.getString("company") + " -> " +
+                    rs3.getInt("number_of_employees"));
+        }
+
+        con.close();
+        st.close();
+        //result1.close();
+
+    }
+}
